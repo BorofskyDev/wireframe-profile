3680d153b7844a656302872e35fd75bd8a078bba
solovyovsdom <joelborofsky@mac.com>
2024-01-10 21:22:07 -0600

    Commented out footer and hero section

--- app/page.jsx
+++ app/page.jsx
@@ -5,11 +5,11 @@ import styles from '../styles/global.scss'
 export default function Home() {
   return (
    <> 
-    <div>[Hero]</div>
+    {/* <div>[Hero]</div>
     <div>[About]</div>
     <div>[Projects]</div>
     <div>[Blog]</div>
-    <div>[Contact]</div>
+    <div>[Contact]</div> */}
    </>
   )
 }
