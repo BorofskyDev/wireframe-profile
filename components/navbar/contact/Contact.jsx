2408c56276c744dc79803641ba2ae61e8e98d278
solovyovsdom <joelborofsky@mac.com>
2024-01-13 16:40:51 -0600

    Add motion effects to ContactButton component

--- components/navbar/contact/Contact.jsx
+++ components/navbar/contact/Contact.jsx
@@ -1,5 +1,38 @@
+'use client'
+
+import { motion } from 'framer-motion'
 import styles from './Contact.module.scss'
 
+const variants = {
+  initial: {
+    color: 'var(--primary-color)',
+    backgroundColor: 'var(--background-color)',
+    transition: { duration: 0.3 },
+  },
+  hover: {
+    color: 'var(--background-color)',
+    backgroundColor: 'var(--highlight-color)',
+    transition: { duration: 0.3, ease: 'easeInOut' },
+  },
+  active: {
+    color: 'var(--background-color)',
+    backgroundColor: 'var(--active-color)',
+    transition: { duration: 0.3, ease: 'easeInOut' },
+  },
+}
+
 export default function ContactButton() {
-  return <a href='#' className={styles.contactButton}>Contact</a>
+  return (
+    <motion.a
+      href='#'
+      className={styles.contactButton}
+      variants={variants}
+      initial='intitial'
+      whileHover='hover'
+      whileFocus='hover'
+      whileTap='active'
+    >
+      Contact
+    </motion.a>
+  )
 }
