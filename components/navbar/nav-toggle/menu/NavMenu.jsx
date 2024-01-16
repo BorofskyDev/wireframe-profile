dc335f13e8bdc401d623e538593a9a9a465a0615
solovyovsdom <joelborofsky@mac.com>
2024-01-16 05:58:28 -0600

    Add styles and update menu items in NavMenu component

--- components/navbar/nav-toggle/menu/NavMenu.jsx
+++ components/navbar/nav-toggle/menu/NavMenu.jsx
@@ -1,21 +1,42 @@
 import NavLink from './NavLink'
 import styles from './NavMenu.module.scss'
+import linkStyles from './NavLink.module.scss'
 
 export default function NavMenu({ isMenuOpen }) {
-  const menuItems = [
-    { name: 'Home', href: '#home' },
-    { name: 'About', href: '#about' },
-    { name: 'Projects', href: '#projects' },
-    { name: 'Blog', href: '#blog' },
-  ]
+  // const menuItems = [
+  //   { name: 'Home', href: '#home' },
+  //   { name: 'About', href: '#about' },
+  //   { name: 'Projects', href: '#projects' },
+  //   { name: 'Blog', href: '#blog' },
+  // ]
 
+  // return (
+  //   <nav className={`${styles.navMenu} ${isMenuOpen ? styles.open : ''}`}>
+  //     <ul>
+  //       {menuItems.map((item) => (
+  //         <NavLink key={item.name} name={item.name} href={item.href} />
+  //       ))}
+  //     </ul>
+  //   </nav>
+  // )
   return (
     <nav className={`${styles.navMenu} ${isMenuOpen ? styles.open : ''}`}>
       <ul>
-        {menuItems.map((item) => (
-          <NavLink key={item.name} name={item.name} href={item.href} />
-        ))}
+        <li className={linkStyles.navLink}>
+          <a href='#/'>Home</a>
+        </li>
+        <li className={linkStyles.navLink}>
+          <a href='#/'>About</a>
+        </li>
+        <li className={linkStyles.navLink}>
+          <a href='#/'>Projects</a>
+        </li>
+        <li className={linkStyles.navLink}>
+          <a href='#/'>Blog</a>
+        </li>
       </ul>
     </nav>
   )
 }
+
+
