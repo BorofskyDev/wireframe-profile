a2fc684862597008b5640e58f4824bc05cef2b07
solovyovsdom <joelborofsky@mac.com>
2024-01-20 08:35:52 -0600

    Add motion animation to external link icon

--- components/links/external-link/ExternalLink.jsx
+++ components/links/external-link/ExternalLink.jsx
@@ -4,6 +4,17 @@ import { motion } from 'framer-motion'
 import styles from './ExternalLink.module.scss'
 
 export default function ExternalLink({ children, href, ...props }) {
+  const icon = {
+    hidden: {
+      pathLength: 0,
+      fill: 'inherit',
+    },
+    visible: {
+      pathLength: 1,
+      fill: 'var(--primary-color)',
+    },
+  }
+
   return (
     <motion.div
       initial={{ opacity: 0, y: 30 }}
@@ -14,22 +25,26 @@ export default function ExternalLink({ children, href, ...props }) {
       <a {...props} href={href} target='_blank'>
         {children}
       </a>
-        <svg
-          width='12'
-          height='12'
-          viewBox='0 0 12 12'
-          fill='none'
-          xmlns='http://www.w3.org/2000/svg'
-        >
-          <path
-            d='M9.11201 1.72043H6.0154V0H12V6.0215H10.2901V2.96774L6.08893 7.19484L4.88004 5.9785L9.11201 1.72043Z'
-            fill='var(--primary-color)'
-          />
-          <path
-            d='M5.12967 1.67773H0V12.0003H10.2593V6.83903H8.54944V10.2799H1.70989V3.39816H5.12967V1.67773Z'
-            fill='#2C1B93'
-          />
-        </svg>
+      <svg
+        width='12'
+        height='12'
+        viewBox='0 0 12 12'
+        fill='none'
+        xmlns='http://www.w3.org/2000/svg'
+      >
+        <motion.path
+          d='M9.11201 1.72043H6.0154V0H12V6.0215H10.2901V2.96774L6.08893 7.19484L4.88004 5.9785L9.11201 1.72043Z'
+          variants={icon}
+          initial='hidden'
+          animate='visible'
+        />
+        <motion.path
+          d='M5.12967 1.67773H0V12.0003H10.2593V6.83903H8.54944V10.2799H1.70989V3.39816H5.12967V1.67773Z'
+          variants={icon}
+          initial='hidden'
+          animate='visible'
+        />
+      </svg>
     </motion.div>
   )
 }
