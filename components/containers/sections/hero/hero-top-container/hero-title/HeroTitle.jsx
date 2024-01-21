941fb864dc2c2998f45d5c06eb93e590e745eb82
solovyovsdom <joelborofsky@mac.com>
2024-01-20 18:15:41 -0600

    Update HeroTitle and NavMenu styles

--- components/containers/sections/hero/hero-top-container/hero-title/HeroTitle.jsx
+++ components/containers/sections/hero/hero-top-container/hero-title/HeroTitle.jsx
@@ -5,7 +5,7 @@ import Heading4 from "@/components/typography/heading4/Heading4";
 export default function HeroTitle() {
     return (
       <TitleWrapper>
-        <Heading1>Fullstack Creative Developer</Heading1>
+        <Heading1>Creative Fullstack Developer</Heading1>
         <Heading4>Design, Develop, Deploy</Heading4>
       </TitleWrapper>
     )
