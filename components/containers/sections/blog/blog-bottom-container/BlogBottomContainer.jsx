b0671d206d889589609d8ce7a51363f01335f84b
solovyovsdom <joelborofsky@mac.com>
2024-01-23 16:36:46 -0600

    Refactor blog bottom container to use getPosts function

--- components/containers/sections/blog/blog-bottom-container/BlogBottomContainer.jsx
+++ components/containers/sections/blog/blog-bottom-container/BlogBottomContainer.jsx
@@ -1,11 +1,35 @@
 import ContentContainer from "@/components/containers/layout-containers/content-container/ContentContainer";
-import BlogPreviewContainer from './blog-preview-container/BlogPreviewContainer';
+import { getPosts } from '@/libs/functions/posts'
+import styles from './BlogPreviewContainer.module.scss'
+import Heading3 from '@/components/typography/heading3/Heading3'
+import BodyText from '@/components/typography/body-text/BodyText'
+import Link from 'next/link'
+import InternalLink from '@/components/links/internal-link/InternalLink'
+import Image from 'next/image'
 
-export default function BlogBottomContainer() {
-   
+export default async function BlogBottomContainer() {
+   const posts = await getPosts()
+
     return (
-        <ContentContainer>
-            <BlogPreviewContainer />
-        </ContentContainer>
+      <ContentContainer>
+        {posts.map((post) => (
+          <div key={post.slug} className={styles.blogPreviewContainer}>
+            <Heading3>{post.title}</Heading3>
+            <Link href={`/blog/${post.slug}`}>
+              <Image
+                src={post.image}
+                alt={post.title}
+                width='320'
+                height='180'
+              />{' '}
+            </Link>
+            <p>{post.date}</p>
+            <BodyText>{post.summary}</BodyText>
+            <InternalLink href={`/blog/${post.slug}`}>
+              Read all of {post.title}
+            </InternalLink>
+          </div>
+        ))}
+      </ContentContainer>
     )
 }
