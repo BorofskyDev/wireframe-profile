f3fdc2f7da342a104d84fc6b83991dfba7df8f6a
solovyovsdom <joelborofsky@mac.com>
2024-03-06 08:06:24 -0600

    Update HeroDesc.jsx and _colors.scss

--- components/containers/sections/hero/hero-top-container/hero-description/HeroDesc.jsx
+++ components/containers/sections/hero/hero-top-container/hero-description/HeroDesc.jsx
@@ -3,10 +3,7 @@ import BodyText from '@/components/typography/body-text/BodyText'
 export default function HeroDesc() {
   return (
     <BodyText>
-      Web designer with a user-first (with an emphasis on accessibility!)
-      approach to design, web app developer with a stack that includes Next.js,
-      SCSS, Nodejs, and MongoDB, and all with a business background in sales and
-      marketing. Also likes cats.
+      Web design, web development, and digital marketing services for businesses and non-profits.
     </BodyText>
   )
 }
